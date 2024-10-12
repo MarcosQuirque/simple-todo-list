7dcd9f0f9b8b44c508cb46c11e3434c9cdb63076
MarcosQ88 <marcosquirque0@gmail.com>
2024-10-12 12:31:06 -0300

    ajustes menores

--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NoteComponent } from '../note/note.component';
 
 @Component({
   selector: 'app-block',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NoteComponent],
   templateUrl: './block.component.html',
   styleUrl: './block.component.scss',
 })
